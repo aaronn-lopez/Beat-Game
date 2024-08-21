5bddf6a2b8b2750c15408a075946a00f01685e40
unknown <Markus.wailee@gmail.com>
2024-08-21 12:48:00 -0700

    Updated DeltaTime class for more readablility, also added a stopwatch

--- src/DeltaTime.hpp
+++ src/DeltaTime.hpp
@@ -1,61 +1,57 @@
 #pragma once
 #include <chrono>
-#include <thread>
-#include <iostream>
 
 
+//Dedicated class for handling loop timing.
 class DeltaTime
 {
-	std::chrono::high_resolution_clock timer;
-	using ms = std::chrono::duration<float, std::milli>;
+    using clock = std::chrono::steady_clock;
+private: //FPS related variables
+    std::chrono::time_point<clock> start_time;
+    long long target_nano = 0;
+    std::chrono::time_point<clock> previous_time;
+    float frame_dt = 0;
+    float current_time = 0;
+private: //stopwatch variables
+    std::chrono::time_point<clock> stopwatch_timer;
 public:
-	DeltaTime() {};
-	void handle_time();
-	void show_fps();
-	float frame_time() { return deltaTime; }
-	float get_time();
-	void set_target_fps(float FPS_TARGET);
-private:
-	std::chrono::time_point<std::chrono::high_resolution_clock> start = timer.now();
-	int fps_counter = 0;
-	float dt_target = 0;
-	float deltaTime = 0;
-	float FPS = 0;
-	float AVG_FPS = 0;
-};
+    DeltaTime();
+    //Handles timing! Place this function inside loops.
+    void handle_time();
 
-inline float DeltaTime::get_time()
-{
-	auto static start_i = timer.now();
-	auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(timer.now() - start_i);
-	return duration_ms.count() / 1000.0f;
-}
+    //Sets the target fps.
+    void set_target_fps(unsigned int fps);
+
+    //Returns the current time between frames in seconds.
+    float frame_time() const;
+    
+    //Returns time passed in seconds.
+    float get_time() const;
+
+    //Returns current fps.
+    float get_fps() const;
+
+public: //stopwatch
+
+    //Starts stopwatch. Use stopwatch_f to finish.
+    void stopwatch_s();
+    //Stops stopwatch and returns duration in ms.
+    float stopwatch_f();
+
+};
+inline DeltaTime::DeltaTime() : previous_time(clock::now()), start_time(clock::now()), target_nano(0){}
+inline float DeltaTime::frame_time() const{ return frame_dt; }
+inline float DeltaTime::get_time() const {return std::chrono::duration<float, std::ratio<1,1>>(clock::now() - start_time).count();}
+inline float DeltaTime::get_fps() const{ return 1.0f/frame_dt; }
+inline void DeltaTime::set_target_fps(unsigned int fps){ target_nano = fps? 1000000000 / fps: 0; }
 inline void DeltaTime::handle_time()
 {
-	//static auto start = timer.now();
-	while (std::chrono::duration_cast<ms>(timer.now() - start).count() < dt_target) {}
-	deltaTime = std::chrono::duration_cast<ms>(timer.now() - start).count() * 0.001;
-	start = timer.now();
+    while(target_nano > std::chrono::duration<long long, std::nano>(clock::now() - previous_time).count()){}
+    frame_dt = std::chrono::duration<float, std::ratio<1,1>>(clock::now() - previous_time).count(); 
+    previous_time = clock::now();
 }
-inline void DeltaTime::show_fps()
-{
-	fps_counter++;
-	FPS += 1 / deltaTime;
-	if (fps_counter >= 100)
-	{
-		//std::cout << FPS / float(100) << '\n';
-		AVG_FPS = FPS / float(100);
-		FPS = 0;
-		fps_counter = 0;
-	}
-	std::cout << AVG_FPS << '\n';
-}
-inline void DeltaTime::set_target_fps(float FPS_TARGET)
-{
-	if (FPS_TARGET == 0)
-	{
-		dt_target = 0;
-		return;
-	}
-	dt_target = 1000 / FPS_TARGET;
-}
+
+
+inline void DeltaTime::stopwatch_s(){ stopwatch_timer = clock::now();}
+inline float DeltaTime::stopwatch_f() { return std::chrono::duration<float, std::milli>(clock::now() - stopwatch_timer).count();}
+
